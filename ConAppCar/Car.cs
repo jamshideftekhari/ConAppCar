efdcc4eb35aafcff98b0d56b0d1c3c41eef02216
Jamshid Eftekhari <jaef@easj.dk>
2015-11-03 21:59:48 +0100

    version 2

---
+++ ConAppCar/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppCar
+{
+    class Car
+    {
+        private int _id;
+    }
+}
